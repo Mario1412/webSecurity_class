5c57a89e8b8b1fa96901f92b4217d963a403a1e8
mario <matianhao0720@gmail.com>
2018-10-07 16:44:47 -0400

    edit level 2 csp rule

--- xss/level2/app.py
+++ xss/level2/app.py
@@ -11,14 +11,7 @@ app = Flask(__name__)
     "img-src": "https://xss-game.appspot.com/static/logos/level2.png "
                "https://xss-game.appspot.com/static/level2_icon.png "
                "https://ssl.gstatic.com/s2/oz/images/sprites/stream-e001443aa61c5529c1aa133a9c12bb49.png",
-    "object-src": "",
-    "plugin-src": "",
-    "style-src": "https://xss-game.appspot.com/static/game-frame-styles.css",
-    "media-src": "",
-    "child-src": "",
-    "connect-src": "",
-    "base-uri": "",
-    "report-uri": ""
+    "style-src": "https://xss-game.appspot.com/static/game-frame-styles.css"
 })
 def index():
     return render_template('index.html')
